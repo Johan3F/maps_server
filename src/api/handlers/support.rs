319d782e8439d898805ec76898ff53450561b347
Johan3F <jvandewallemuniz@gmail.com>
2022-03-24 21:04:08 +0000

    Added unit test for health endpoint

--- src/api/handlers/support.rs
+++ src/api/handlers/support.rs
@@ -2,3 +2,18 @@ use rocket::get;
 
 #[get("/health")]
 pub fn health() {}
+
+#[cfg(test)]
+mod test {
+    use crate::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn health_endpoint() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/health").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string(), None)
+    }
+}
