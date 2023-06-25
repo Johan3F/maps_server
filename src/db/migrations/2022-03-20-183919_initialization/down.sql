6f94ff200fd19387b9626a631500aa40e1b35f60
Johan3F <jvandewallemuniz@gmail.com>
2023-06-25 16:41:12 +0000

    fixes down migrations

--- src/db/migrations/2022-03-20-183919_initialization/down.sql
+++ src/db/migrations/2022-03-20-183919_initialization/down.sql
@@ -1,5 +1,5 @@
-DROP TABLE collections;
-DROP TABLE elements;
 DROP TABLE points;
 DROP TABLE tracks;
 DROP TABLE geometries;
+DROP TABLE elements;
+DROP TABLE collections;
--- src/db/schema.rs
+++ src/db/schema.rs
@@ -6,3 +6,40 @@ diesel::table! {
         name -> Text,
     }
 }
+
+diesel::table! {
+    elements (id) {
+        id -> Uuid,
+        collection_id -> Nullable<Uuid>,
+        name -> Text,
+        description -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    geometries (element_id) {
+        element_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    points (element_id) {
+        element_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    tracks (element_id) {
+        element_id -> Uuid,
+    }
+}
+
+diesel::joinable!(elements -> collections (collection_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    collections,
+    elements,
+    geometries,
+    points,
+    tracks,
+);
